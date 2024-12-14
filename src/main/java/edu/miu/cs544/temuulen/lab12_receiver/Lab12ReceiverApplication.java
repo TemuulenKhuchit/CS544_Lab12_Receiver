21d04b20a3fe18e1cf4e1e6ecacfc829ffbc2c1b
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-13 21:27:23 -0600

    Initial commit

---
+++ src/main/java/edu/miu/cs544/temuulen/lab12_receiver/Lab12ReceiverApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.temuulen.lab12_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab12ReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab12ReceiverApplication.class, args);
+    }
+
+}
